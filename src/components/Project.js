03494ce7f94b6dbc3527c05a465960b3ddf77b3c
Kunal Gaurav <kunalgaurav0405@gmail.com>
2020-08-21 15:02:58 +0530

    feaured projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,8 +2,33 @@ import React from "react"
 import PropTypes from "prop-types"
 import Image from "gatsby-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = () => {
-  return <h2>project copmonent</h2>
+const Project = ({ description, title, url, image, github, stack, index }) => {
+  return (
+    <article className="project">
+      <Image
+        fluid={image.childImageSharp.fluid}
+        className="project-img"
+      ></Image>
+      <div className="project-info">
+        <span className="project-number">0{index + 1}</span>
+        <h3>{title}</h3>
+        <p className="project-desc">{description}</p>
+        <div className="project-stack">
+          {stack.map(item => {
+            return <span key={item.id}>{item.title}</span>
+          })}
+        </div>
+        <div className="project-links">
+          <a href={github}>
+            <FaGithubSquare className="project-icon"> </FaGithubSquare>
+          </a>
+          <a href={url}>
+            <FaShareSquare className="project-icon"> </FaShareSquare>
+          </a>
+        </div>
+      </div>
+    </article>
+  )
 }
 
 Project.propTypes = {}
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,45 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 import Stack from "../components/Stack"
-export default () => {
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
   return (
     <Layout>
       <Hero></Hero>
       <Services></Services>
       <Jobs></Jobs>
       <Stack></Stack>
+      <Projects
+        projects={projects}
+        title="Fearured Projects"
+        showLink
+      ></Projects>
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
